fbec78b04ebbfa1508bec63325a87689b70d1695
sbsto <37947148+sbsto@users.noreply.github.com>
2025-02-03 00:53:05 +0000

    Remove 1password from nix

--- hosts/shared.nix
+++ hosts/shared.nix
@@ -1,10 +1,5 @@
 { config, pkgs, ... }: {
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  programs = {
-    zsh.enable = true;
-    _1password.enable = true;
-  };
-
   nixpkgs.config.allowUnfree = true;
 }
