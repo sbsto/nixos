7dcc17f8ccf4f539dd135b26ecc61e592ff11090
Sam Brownstone <brownstone@hey.com>
2025-03-15 20:43:35 +0000

    Remove macOS stuff

--- flake.nix
+++ flake.nix
@@ -5,7 +5,6 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    darwin.url = "github:LnL7/nix-darwin";
   };
 
   outputs = { nixpkgs, home-manager, darwin, ... }: {
@@ -19,24 +18,11 @@
           {
             home-manager.useUserPackages = true;
             home-manager.useGlobalPkgs = true;
-            home-manager.backupFileExtension = "backup2";
+            home-manager.backupFileExtension = "backup";
 
             home-manager.users.sbsto = import ./home/nixos/sbsto.nix;
             home-manager.users.tonytarizzo = import ./home/nixos/tonytarizzo.nix;
             home-manager.users.wannabehero = import ./home/nixos/wannabehero.nix;
-          }
-        ];
-      };
-    };
-
-    darwinConfigurations = {
-      alphabook = darwin.lib.darwinSystem {
-        system = "aarch64-darwin";
-        modules = [
-          ./hosts/macos/configuration.nix
-          home-manager.darwinModules.home-manager
-          {
-            home-manager.users.sambrownstone = import ./home/macos.nix;
           }
         ];
       };
